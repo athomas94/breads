c86fde5aabdd88f6a6a9f70052f7c4e4eee58767
Andrew Thomas <athomas94@gmail.com>
2024-01-30 20:52:58 -0500

    added various mongo related changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 
 require('dotenv').config()
@@ -19,6 +20,10 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
+//Mongo connection
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
+
+//Controllers
 const breadsController = require('./controllers/breads_controllers')
 app.use('/breads', breadsController)
 
@@ -27,6 +32,7 @@ app.get('*', (req, res) => {
     res.status(404).send('404')
 })
 
+//LISTENERS
 app.listen(PORT, () => {
     console.log('listening on port ' + PORT)
 })
