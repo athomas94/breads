a2bde8a677d92c68f0dcc336ae70f8d1b39a0eb8
Andrew Thomas <athomas94@gmail.com>
2024-02-04 12:42:21 -0500

    Added virtuals and created a view for all of the bakers on the index page and subsiquent child pages

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -10,6 +10,25 @@ baker.get('/data/seed', (req, res) => {
         .then(res.redirect('/breads'))
 })
 
+//Index
+baker.get('/', (req, res) => {
+    Baker.find()
+    .populate('breads')
+    .then(foundBakers => {
+        res.send(foundBakers)
+    })
+})
+
+//SHOW
+baker.get('/:id', (req,res) => {
+    Baker.findById(req.params.id)
+    .populate('breads')
+    .then(foundBaker => {
+        res.render('bakerShow', {
+            baker: foundBaker
+        })
+    })
+})
 
 //Export
 module.exports = baker
--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -5,14 +5,18 @@ const Baker = require('../models/baker.js')
 
 //INDEX
 breads.get('/', (req,res) => {
+  Baker.find()
+  .then(foundBakers => {
     Bread.find()
       .then(foundBreads => {
         res.render('Index', 
         {
             breads: foundBreads,
+            bakers: foundBakers,
             title: 'Index Page'
         })
       })
+    })
 })
 
 //NEW
